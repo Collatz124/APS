18ffe3624a45c827d0aeee76c9ed4258bfbea5ad
Martin Sig Nørbjerg <martin1509@outlook.dk>
2020-11-27 13:40:09 +0100

    Prøv igen markus

--- flights.py
+++ flights.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def GenerateLandingDuration():
     """ Genere en landings tid baseret på det givne data fra oplægget. """
-    dist = (0, 16, 33, 61, 41, 25, 10, 8, 6, 0)  # Hvert index repreæsentere en landingstid i sekunder 0: 0-30, 1: 31-60 osv.
+    dist = [0, 16, 33, 61, 41, 25, 10, 8, 6, 0]  # Hvert index repreæsentere en landingstid i sekunder 0: 0-30, 1: 31-60 osv.
 
-    randomNumber = np.random.randint(0, sum(dist) + 1)  # Genenere et tilfældigt tal, for at finde ud af i hvilket område flyets landings tid skal ligge
+    randomNumber = np.random.randint(0, sum(dist) + 1)  # Genenere et tilfældigt tal, for at finde ud af i hvilket område flyets landings tid skal ligge i.
     for i in range(len(dist)):
-        if randomNumber >= sum(dist[:i]): # Hvis det tilfældige tal lige har været i sum(dist[:i]) generes et tilfældigt tal nedenfor
+        if randomNumber <= sum(dist[:i]): # Hvis det tilfældige tal lige har været i sum(dist[:i]) generes et tilfældigt tal nedenfor
             return np.random.randint(30 * i + 1 if (i > 0) else 0, 30 * (i + 1))  # Tager højde for at distruptionen kan ændre sig så der kommer fly med landingstider på 0-30 sekunder
 
 
