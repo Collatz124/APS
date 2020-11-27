2ea05403c3b694e9a3def0fcf7496fbb8e2acdda
Martin Sig Nørbjerg <martin1509@outlook.dk>
2020-11-27 14:08:25 +0100

    Opdaterede aksetitler

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ years = int(input("Antal år, efter år 0: ")) + 1 # Pluser med 1 så
 skipYears = int(input("Hvor mange år vil du have imellem hver simulering? "))
 days = int(input("Antal dage der skal simuleres pr år: "))
 offset = int(input("Offset, bruges til at simulere dårligt landingsvejr og er en konstant der ligges til hvert flys landingsvarrighed. Sæt denne vaerdi til 0, ved almindeligt vejr... "))
-print("Påbegynder simulering...") # Så det kan ses at simuleringen går igang da den kan tage langt tid.
+print("\nPåbegynder simulering...") # Så det kan ses at simuleringen går igang da den kan tage langt tid.
 
 start = time()
 average, highest = runSimulations(years, days, skipYears, offset)  # Denne funktion returnere en tuple denne bliver derfor pakket ud i to variabler
@@ -18,9 +18,9 @@ average, highest = runSimulations(years, days, skipYears, offset)  # Denne funkt
 print("Det tog {0} sekunder...".format(time() - start)) # Tiden simuleringen tog.
 
 # Plotter data fra simuleringen
-plotData(average, years, skipYears, ("Gennemsnitlig total ventetid", "Antal år", "Gennemsnitlig totalventetid i sekunder"))
-plotData(highest, years, skipYears, ("Højeste ventetid", "Antal år", "Højeste ventetid i sekunder"))
+plotData(1 / 60 * np.array(average), years, skipYears, ("Gennemsnitlig total ventetid", "Antal år", "Gennemsnitlig totalventetid i minutter"))
+plotData(1 / 60 * np.array(highest), years, skipYears, ("Højeste ventetid pr år", "Antal år", "Højeste ventetid i minutter"))
 
 # Beregner gennemsnitlig vente tid pr fly udfra variablen average.
-averagePrFlight = [[avrg / (200 * np.power(1.05, year)) for avrg, year in zip(average[k], range(0, years, skipYears))] for k in range(2)] # Beregner gennemsnitet pr fly pr år.
-plotData(averagePrFlight, years, skipYears, ("Gennemsnitlig ventetid", "Antal år", "Gennemsnitlig ventetid i sekunder"))
+averagePrFlight = [[avrg / (60 * 200 * np.power(1.05, year)) for avrg, year in zip(average[k], range(0, years, skipYears))] for k in range(2)] # Beregner gennemsnitet pr fly pr år.
+plotData(averagePrFlight, years, skipYears, ("Gennemsnitlig ventetid pr fly", "Antal år", "Gennemsnitlig ventetid pr fly i minutter"))
