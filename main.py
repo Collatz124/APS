694e5bbb2e32cf673c6e7609f46ad8a22324328a
Martin Sig Nørbjerg <martin1509@outlook.dk>
2020-11-27 12:57:17 +0100

    Ændrede i main for at give en kort introduktion

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from viz import plotData
 from time import time
 import numpy as np
 
-years = int(input("Antal aar, efter aar 0: ")) + 1
-skipYears = int(input("Hvor mange aar vil du have imellem hver simulering? "))
-days = int(input("Antal dage der skal simuleres pr aar: "))
-offset = int(input("Offset, bruges til at simulere daarligt landingsvejr og er en konstant der ligges til hvert flys landingsvarrighed. Sæt denne vaerdi til 0, ved almindeligt vejr..."))
-print("Paabegynder simulering...") # Så det kan ses at simuleringen går igang da den kan tage langt tid.
+print("Hvis du ønsker at køre koden for at teste om den virker anbefalder vi instillingerne: \n - År: 5,\n - skipYears: 1,\n, - Dage pr år: 10,\n - offset: 0")
+years = int(input("Antal år, efter år 0: ")) + 1
+skipYears = int(input("Hvor mange år vil du have imellem hver simulering? "))
+days = int(input("Antal dage der skal simuleres pr år: "))
+offset = int(input("Offset, bruges til at simulere dårligt landingsvejr og er en konstant der ligges til hvert flys landingsvarrighed. Sæt denne vaerdi til 0, ved almindeligt vejr... "))
+print("Påbegynder simulering...") # Så det kan ses at simuleringen går igang da den kan tage langt tid.
 
 start = time()
 average, highest = runSimulations(years, days, skipYears, offset)  # Denne funktion returnere en tuple denne bliver derfor pakket ud i to variabler
@@ -17,9 +18,9 @@ average, highest = runSimulations(years, days, skipYears, offset)  # Denne funkt
 print("Det tog {0} sekunder...".format(time() - start)) # Tiden simuleringen tog.
 
 # Plotter data fra simuleringen
-plotData(average, years, skipYears, ("Gennemsnitlig total ventetid", "Antal aar", "Gennemsnitlig totalventetid i sekunder"))
-plotData(highest, years, skipYears, ("Hoejeste ventetid", "Antal aar", "Hoejeste ventetid i sekunder"))
+plotData(average, years, skipYears, ("Gennemsnitlig total ventetid", "Antal år", "Gennemsnitlig totalventetid i sekunder"))
+plotData(highest, years, skipYears, ("Højeste ventetid", "Antal år", "Højeste ventetid i sekunder"))
 
-# Beregner gennemsnitlig vente tid pr fly udfra variablen average. TODO: få den her del til at fungere
+# Beregner gennemsnitlig vente tid pr fly udfra variablen average.
 averagePrFlight = [[avrg / (200 * np.power(1.05, year)) for avrg, year in zip(average[k], range(0, years, skipYears))] for k in range(2)] # Beregner gennemsnitet pr fly pr år.
-plotData(averagePrFlight, years, skipYears, ("Gennemsnitlig ventetid", "Antal aar", "Gennemsnitlig ventetid i sekunder"))
+plotData(averagePrFlight, years, skipYears, ("Gennemsnitlig ventetid", "Antal år", "Gennemsnitlig ventetid i sekunder"))
